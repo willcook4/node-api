36c85df4bd754542d8c430415b6952bc57270df5
Will Cook <nzwillcook@gmail.com>
2016-10-21 09:45:41 +0100

    started to setup config files

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ let options = {
   replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 3000 } }
   };
 
+// Database connection
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// Don't show the log when in test
+if(config.util.getEnv('NODE_ENV') !== 'test'){
+  // morgan setup (logging)
+  app.use(morgan('tiny'));
+}
